c2517537eb47cf302adacc869dcaab76711964d0
Kto_I_takoi <ponosyzbeka99@gmail.com>
2023-06-23 21:26:50 +0300

    xc

--- oop_230623/IP.cpp
+++ oop_230623/IP.cpp
@@ -11,13 +11,13 @@ IP IP::ip(unsigned char octet1, unsigned char octet2, unsigned char octet3, unsi
 
 bool IP::ValidIP(IP* ip)
 {
-    if (_Octet1 < 0 && _Octet1 < 256);
+    if (_Octet1 < 0 && _Octet1 < 255);
     else
-        if (_Octet2 < 0 && _Octet2 < 256);
+        if (_Octet2 < 0 && _Octet2 < 255);
         else
-            if (_Octet3 < 0 && _Octet3 < 256);
+            if (_Octet3 < 0 && _Octet3 < 255);
             else {
-                if (_Octet4 < 0 && _Octet4 < 256);
+                if (_Octet4 < 0 && _Octet4 < 255);
                 return true;
             }
 
@@ -26,7 +26,7 @@ bool IP::ValidIP(IP* ip)
 
 IP IP::NewIP(NetMask* netmask)
 {
-    IP newip("255","220","255","4")
+    IP newip("255", "220", "255", '4');
 
     return newip();
 }
